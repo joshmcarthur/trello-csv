071a2fe3f7aa8740e49cbf031b67565aaea253ef
Josh McArthur <joshua.mcarthur@gmail.com>
2012-03-14 16:33:59 +1300

    Add card description to export

--- coffeescripts/trello_csv_export.coffee
+++ coffeescripts/trello_csv_export.coffee
@@ -23,13 +23,14 @@ class TrelloCSVExport
 
   board_to_csv: ->
     @csv_string = ""
+    @csv_sep = ","
     @line_ending = "\n"
-    Trello.boards.get "#{@board.id}/lists", { cards: 'open', card_fields: 'name', fields: 'name'}, (lists) =>
+    Trello.boards.get "#{@board.id}/lists", { cards: 'open', card_fields: 'name,desc', fields: 'name'}, (lists) =>
       $.each lists, (index, list) =>
-        @csv_string += @line_ending
-        @csv_string += "#{list.name}#{@line_ending}"
+        @csv_string += (@csv_sep + @csv_sep + @line_ending) #Newline
+        @csv_string += "#{list.name}#{@csv_sep}#{@line_ending}"
         $.each list.cards, (index, card) =>
-          @csv_string += "#{card.name}#{@line_ending}"
+          @csv_string += "#{card.name}#{@csv_sep}#{card.desc}#{@line_ending}"
       # #TODO replace with jquery.savefile
       $('<form></form>', { action: "http://savefile.herokuapp.com/#{@board.name}.csv", method: 'post'}).append(
         $('<input></input>', { type: 'hidden', name: 'content', value: @csv_string })
--- javascripts/trello_csv_export.js
+++ javascripts/trello_csv_export.js
@@ -36,21 +36,22 @@
     TrelloCSVExport.prototype.board_to_csv = function() {
       var _this = this;
       this.csv_string = "";
+      this.csv_sep = ",";
       this.line_ending = "\n";
       return Trello.boards.get("" + this.board.id + "/lists", {
         cards: 'open',
-        card_fields: 'name',
+        card_fields: 'name,desc',
         fields: 'name'
       }, function(lists) {
         $.each(lists, function(index, list) {
-          _this.csv_string += _this.line_ending;
-          _this.csv_string += "" + list.name + _this.line_ending;
+          _this.csv_string += _this.csv_sep + _this.csv_sep + _this.line_ending;
+          _this.csv_string += "" + list.name + _this.csv_sep + _this.line_ending;
           return $.each(list.cards, function(index, card) {
-            return _this.csv_string += "" + card.name + _this.line_ending;
+            return _this.csv_string += "" + card.name + _this.csv_sep + card.desc + _this.line_ending;
           });
         });
         return $('<form></form>', {
-          action: "http://savefile.joshmcarthur.com/" + _this.board.name + ".csv",
+          action: "http://savefile.herokuapp.com/" + _this.board.name + ".csv",
           method: 'post'
         }).append($('<input></input>', {
           type: 'hidden',
